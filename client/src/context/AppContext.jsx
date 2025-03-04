0b108e70346af1418c42ffcdd5f9f947100923fa
Kaveesha Dissanayake <90174106+kaveeshahq@users.noreply.github.com>
2025-03-04 10:37:00 +0530

    Folder Structure Creation

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,16 @@
+import { createContext, useContext } from "react";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = (props) => {
+
+    const value = {
+
+    };
+     
+   return (
+    <AppContext.Provider value={value}>
+        {props.children}
+    </AppContext.Provider>
+   )
+}
